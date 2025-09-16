6e6876a49578c2aaf9a583f65bbb3b0779b0e32a
kaziama <professionalgith@gmail.com>
2025-09-16 20:46:39 +0200

    pushing packages

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 // Mobile/firebase.js
-import { initializeApp } from "firebase/app";
+/*import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase, ref, set } from "firebase/database";
 
@@ -27,5 +27,41 @@ export const saveLocation = async (userId, location) => {
     lng: location.coords.longitude,
     ts: Date.now(),
   });
+};*/
+// Firebase new version with persistance
+
+// Mobile/firebase.js
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDGo_P0fJ1j1HrZ1hpi9Uyd3nWnIBk6Bw",
+  authDomain: "live-tracking-gps-technoweb.firebaseapp.com",
+  databaseURL: "https://live-tracking-gps-technoweb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "live-tracking-gps-technoweb",
+  storageBucket: "live-tracking-gps-technoweb.appspot.com",
+  messagingSenderId: "26999020261",
+  appId: "1:26999020261:web:5295385342531fbe70520c"
 };
 
+// Initialiser l’app Firebase
+const app = initializeApp(firebaseConfig);
+
+// ✅ Auth avec persistance AsyncStorage (important en React Native)
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+// ✅ Database
+export const db = getDatabase(app);
+
+// Exemple utilitaire pour sauvegarder la position
+export const saveLocation = async (userId, location) => {
+  await set(ref(db, "locations/" + userId), {
+    lat: location.coords.latitude,
+    lng: location.coords.longitude,
+    ts: Date.now(),
+  });
+};
